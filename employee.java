550b45b65facb817655522650c169ecf8716ab5f
root <root@ip-172-31-26-227.ec2.internal>
2026-07-29 13:05:41 +0000

    javacode

---
+++ employee.java
@@ -0,0 +1,8 @@
+public class employee {
+    public static void main(String[] args) {
+        int empid=1010;
+        String name ="sai";
+        System.out.println("My EmployeeId is "+empid);
+        System.out.println("My Name is "+name);
+    }
+}
